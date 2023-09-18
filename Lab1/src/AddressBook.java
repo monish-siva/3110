f28a43d814d8567c9993ca51941f8ec2536d3ea1
Monishkumar Sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2023-09-18 12:06:34 -0400

    Lab 2 git changes

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -20,7 +20,6 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
-        System.out.println("Address Book");
     }
 
 }
