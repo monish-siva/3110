e52dfa9224dfc1ff986549785183f4bf13a7e0a5
Monishkumar Sivakumar <63323924+monishkumar-sivakumar@users.noreply.github.com>
2023-09-18 12:05:26 -0400

    Lab 2 git changes

---
+++ Lab1/src/AddressBook.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddies;
+
+    public AddressBook(){
+        this.buddies = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        this.buddies.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy){
+        this.buddies.remove(buddy);
+    }
+
+    public static void main(String [] args){
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+        System.out.println("Address Book");
+    }
+
+}
